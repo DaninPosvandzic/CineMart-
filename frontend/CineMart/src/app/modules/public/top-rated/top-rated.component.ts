7a19c7fcde7fa6a2aa3915730b59db317bf292bd
mirza-z <mirza.zaimovic@edu.fit.ba>
2026-01-08 16:53:00 +0100

    41 - Navbar update

--- frontend/CineMart/src/app/modules/public/top-rated/top-rated.component.ts
+++ frontend/CineMart/src/app/modules/public/top-rated/top-rated.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {FilmService} from '../../../api-services/filmManagement/film-api.service';
-import {Film} from '../../../api-services/filmManagement/film-api.model';
-
+import { FilmService } from '../../../api-services/filmManagement/film-api.service';
+import { Film } from '../../../api-services/filmManagement/film-api.model';
 
 @Component({
   selector: 'top-rated',
-  standalone:false,
+  standalone: false,
   templateUrl: './top-rated.component.html',
   styleUrls: ['./top-rated.component.scss'],
 })
@@ -21,20 +20,15 @@ export class TopRatedComponent implements OnInit {
         this.allFilms = res;
         this.sortByRating();
       },
-      error: (err:unknown) => {
+      error: (err: unknown) => {
         console.error('Greška pri dohvaćanju filmova:', err);
       }
     });
   }
 
-  /** Sortira filmove po averageRating i uzima top 10 */
   sortByRating() {
     this.topRatedMovies = [...this.allFilms]
       .sort((a, b) => b.averageRating - a.averageRating)
-      .slice(0, 10)
-      .map(film => ({
-        ...film,
-        rating: Number(film.averageRating? film.averageRating.toFixed(2): 2.50) // zaokruži na 2 decimale
-      }));
+      .slice(0, 10);
   }
 }
